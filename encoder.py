c209e0a690a8819f1de34c133c7ff62cea94b6b6
juliansanchez6 <julian.sanchez6@udea.edu.co>
2024-03-05 12:56:41 -0500

    Encoder listo

--- encoder.py
+++ encoder.py
@@ -27,7 +27,7 @@
 
 
 morse_code_dict = {
-    ".-.-.": "A",
+    "A": ".-.-.",
     "-...": "B",
     "-.-.": "C",
     "-..": "D",
@@ -66,3 +66,45 @@ morse_code_dict = {
     ".-": "#",  # Espacio en negro
     ".": "$",  # Espacio en blanco
 }
+
+
+def text_to_morse(text):
+    text = text.upper()  # Convert text to uppercase
+    morse_code_func = []
+
+    for char in text:
+        if char == " ":
+            morse_code_func.append(" ")
+        elif char in morse_code_dict:
+            morse_code_func.append(morse_code_dict[char])
+        else:
+            morse_code_func.append(char)  # Keep unknown characters as is
+
+    return "   ".join(morse_code_func)
+
+
+rows = int(input("Ingrese la cantidad de filas: "))
+cols = int(input("Ingrese la cantidad de columnas: "))
+
+matriz = [[" " for _ in range(rows)] for _ in range(cols)]
+
+count_rows = 0
+count_cols = 0
+
+while count_rows < rows:
+
+    while count_cols < cols:
+        chrt_in = input(f"Enter character for row and col({count_rows},{count_cols}): ")
+        matriz[count_rows][count_cols] = chrt_in
+
+        # Codificar el mensaje
+        morse_code = text_to_morse(chrt_in)
+        print(f"Code to transmit: {morse_code}")
+        count_cols += 1
+
+    count_cols = 0
+    count_rows += 1
+
+print("Matriz Resultante:")
+for fila in matriz:
+    print(" ".join(map(str, fila)))
